b612e7a2a762df8de81a20b8dd68881d2053cda6
Filipe Davi <filipedavi80@gmail.com>
2022-05-29 12:43:41 -0300

    update scripts.jsx

--- src/scripts.jsx
+++ src/scripts.jsx
@@ -1,9 +1,9 @@
-const K = document.getElementById('K');
-const L = document.getElementById('L');
-const E = document.getElementById('E');
-const TE = document.getElementById('TE');
+const K = document.getElementById('K')
+const L = document.getElementById('L')
+const E = document.getElementById('E')
+const TE = document.getElementById('TE')
 
-const result = document.getElementById('result');
+const result = document.getElementById('result')
 
 class Equations {
     constructor(cond_termica, lado, espessura, temp_externa) {
@@ -14,26 +14,26 @@ class Equations {
     }
 
     fluxoCalor() {
-        let fluxo = this.cond_termica * 6 * Math.pow(this.lado, 2) * Math.abs(this.temp_externa - 25);
-        fluxo = fluxo / this.espessura;
+        let fluxo = this.cond_termica * 6 * Math.pow(this.lado, 2) * Math.abs(this.temp_externa - 25)
+        fluxo = fluxo / this.espessura
 
         /*console.log(this.cond_termica);
         console.log(this.lado);
         console.log(this.temp_externa);
         console.log(this.espessura);*/
 
-        return fluxo;
+        return fluxo
     }
 
     resultadoWatt() {
-        let watt = this.fluxoCalor() * 4.184;
+        let watt = this.fluxoCalor() * 4.184
         
         return watt
     }
 }
 
-let potencia = new Equations(2, 2, 3, 25);
-console.log(potencia.resultadoWatt());
+let potencia = new Equations(2, 2, 3, 25)
+console.log(potencia.resultadoWatt())
 
 
 
@@ -42,23 +42,23 @@ function calculate() {
 
     document.getElementById('result').innerHTML = JSON.stringify(potencia.resultadoWatt())*/
 
-    let cond_termica = K.value;
-    let lado = L.value;
-    let espessura = E.value;
-    let temp_externa = TE.value;
+    let cond_termica = K.value
+    let lado = L.value
+    let espessura = E.value
+    let temp_externa = TE.value
 
-    let potencia = new Equations(cond_termica, lado, espessura, temp_externa);
+    let potencia = new Equations(cond_termica, lado, espessura, temp_externa)
     
-    console.log(isNaN(potencia.resultadoWatt()));
+    console.log(isNaN(potencia.resultadoWatt()))
 
     if (isNaN(potencia.resultadoWatt()) == false) {
         if (temp_externa == 25) 
             result.innerHTML = "Ops! A potência será de 0 W. A temperatura externa é a mesma que 25°C ;)"
 
         else if (temp_externa < 25) 
-            result.innerHTML = `A potência necessária será de ${potencia.resultadoWatt().toFixed(1)} W`;
-        else result.innerHTML = "Erro: A temperatura externa está superior a 25°C!";
+            result.innerHTML = `A potência necessária será de ${potencia.resultadoWatt().toFixed(1)} W`
+        else result.innerHTML = "Erro: A temperatura externa está superior a 25°C!"
     }
         
-    else result.innerHTML = "Erro: Insira os dados corretamente!";
+    else result.innerHTML = "Erro: Insira os dados corretamente!"
 }
