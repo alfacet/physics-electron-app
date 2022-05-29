da4b41e426393bd192db41370d96ec787b36bcfc
Filipe Davi <filipedavi80@gmail.com>
2022-05-29 19:44:36 -0300

    add .exe

---
+++ physics-app-win32-x64/resources/app/index.js
@@ -0,0 +1,58 @@
+const {app, BrowserWindow, Menu} = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        
+        icon: __dirname + '/doge.png'
+    });
+
+
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const mainMenuTemplate = [
+    {
+        label: 'Bem-vindo!'
+    },
+    {
+        label: 'Autores',
+        submenu: [
+            {
+                label: 'Daniel Lopes'
+            },
+            {
+                label: 'Filipe Davi'
+            },
+            {
+                label: 'Samuel Gadoni'
+            },
+            {
+                label: 'Luís Fernando'
+            }
+        ]
+    },
+    {
+        label: 'Participantes', 
+        submenu: [
+            {
+                label: 'Sidão ao ministrar as aulas de termologia'
+            },
+            {
+                label: 'Documentação do electron'
+            },
+            {
+                label: 'Documentação do JavaScript'
+            },
+            {
+                label: 'Caderno de rascunho do Mário'
+            }
+        ],
+    }
+];
